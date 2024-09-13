2ee713cb1394bbfe95b6f73de82c7167df5b3d8e
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-09-13 03:15:08 -0400

    updated tests and passed test cases

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def count_a_letter(sentence, letter):
         return None
     if not sentence:
         return None
+    if len(letter) > 1:
+        return "Enter one letter only"
     
     count = 0
     for char in sentence:
--- tests/test_count_a_letter.py
+++ tests/test_count_a_letter.py
@@ -1,19 +1,53 @@
 from main import count_a_letter
 import pytest
 
-def test_demo_one():
-    num_1 = 8
-    num_2 = 9
+# def test_multiple_letters_input_returns_error():
+#     letter = "ll"
+#     sentence = "Hello world."
+#     assert count_a_letter(sentence, letter) == "Enter one letter only"
 
-    result = num_1 + num_2
+# def test_letters_are_calculated_correctly():
+#     letter = "l"
+#     sentence = "Hello world."
+#     assert count_a_letter(sentence, letter) == 3
 
-    assert result == 17
+# def test_letter_does_not_appear_in_sentence():
+#     letter = "a"
+#     sentence = "Hello world."
+#     assert count_a_letter(sentence, letter) == 0
 
-def test_demo_two():
-    num_1 = 18
-    num_2 = 24
+# def test_letter_is_not_alpha():
+#     letter = ";"
+#     sentence = "Hello world."
+#     assert count_a_letter(sentence, letter) == None
 
-    result = num_1 + num_2
 
-    assert result == 42
+# def test_invalid_sentence_returns_none():
+#     letter = "a"
+#     sentence = ""
+#     assert count_a_letter(sentence, letter) == None
+
+# def test_letters_are_calculated_correctly():
+#     letter = "3"
+#     sentence = "Hello world."
+#     result = count_a_letter(sentence, letter)    
+
+#     assert result == 
+
+
+# def test_demo_one():
+#     num_1 = 8
+#     num_2 = 9
+
+#     result = num_1 + num_2
+
+#     assert result == 17
+
+# def test_demo_two():
+#     num_1 = 18
+#     num_2 = 24
+
+#     result = num_1 + num_2
+
+#     assert result == 42
 # Delete the demo tests and add your tests here 
